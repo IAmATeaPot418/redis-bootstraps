ae4fe5f5ea5e500eb3f2961dc8e04e06843473cf
Jamie Scott <5336227+IAmATeaPot418@users.noreply.github.com>
2019-10-16 13:50:38 -0700

    Update redisoss.sh

--- redisoss.sh
+++ redisoss.sh
@@ -9,7 +9,5 @@ cd ./redis
 make BUILD_TLS=yes
 ./utils/gen-test-certs.sh
 ./runtest --tls
-./src/redis-server --tls-port 6379 --port 0 \
- --tls-cert-file ./tests/tls/redis.crt \
---tls-key-file ./tests/tls/redis.key \
---tls-ca-cert-file ./tests/tls/ca.crt
+git clone https://github.com/IAmATeaPot418/redis-bootstraps.git
+./src/redis-server ./redis-bootstraps/redis.conf
