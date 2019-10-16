c0b65eebf59789e44c8aec2f9cd0756b8bb8eec3
Jamie Scott <5336227+IAmATeaPot418@users.noreply.github.com>
2019-10-16 16:45:28 -0700

    Update redisoss.sh

--- redisoss.sh
+++ redisoss.sh
@@ -4,10 +4,13 @@ sudo apt install build-essential tcl -y
 sudo apt install tcl-tls -y
 sudo apt install libssl-dev -y
 sudo apt install redis-tools -y
+useradd redisuser 
 git clone https://github.com/antirez/redis.git
 cd ./redis
 make BUILD_TLS=yes
 ./utils/gen-test-certs.sh
 ./runtest --tls
-git clone https://github.com/IAmATeaPot418/redis-bootstraps.git
-./src/redis-server ./redis-bootstraps/redis.conf
+./src/redis-server --tls-port 6379 --port 0 \
+        --tls-cert-file ./tests/tls/redis.crt \
+        --tls-key-file ./tests/tls/redis.key \
+        --tls-ca-cert-file ./tests/tls/ca.crt
