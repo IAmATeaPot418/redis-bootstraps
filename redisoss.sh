1e770132cef12e766a8a887bc9e7f65bfb31f778
Jamie Scott <5336227+IAmATeaPot418@users.noreply.github.com>
2019-10-16 12:59:23 -0700

    changes to update to make bootstrap require TLS

--- redisoss.sh
+++ redisoss.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-sudo apt install redis-tools -y
 sudo sudo apt update -y
 sudo apt install build-essential tcl -y
+sudo apt install tcl-tls -y
+sudo apt install libssl-dev -y
+sudo apt install redis-tools -y
 git clone https://github.com/antirez/redis.git
 cd ./redis
-make
-cd ./src
-./redis-server
+make BUILD_TLS=yes
+./utils/gen-test-certs.sh
+./runtest --tls
+./src/redis-server --tls-port 6379 --port 0 \
+ --tls-cert-file ./tests/tls/redis.crt \
+--tls-key-file ./tests/tls/redis.key \
+--tls-ca-cert-file ./tests/tls/ca.crt
