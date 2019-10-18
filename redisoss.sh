8476fe9887e4c9fd1cf6415dad6504948dda528a
Jamie Scott <5336227+IAmATeaPot418@users.noreply.github.com>
2019-10-18 15:50:24 -0700

    Update redisoss.sh

--- redisoss.sh
+++ redisoss.sh
@@ -2,9 +2,7 @@
 sudo sudo apt update -y
 sudo apt install build-essential tcl -y
 sudo apt install tcl-tls -y
-sudo apt install libssl-dev -y
 sudo apt install redis-tools -y
-useradd redisuser 
 git clone https://github.com/antirez/redis.git
 cd ./redis
 make BUILD_TLS=yes
